e381a0a5b1a4a437e05b6e7610f7b48b28472686
salmakishk98 <salma.mohamed2255@gmail.com>
2021-05-29 19:30:19 +0200

    edit

--- javascript/medicine.js
+++ javascript/medicine.js
@@ -1,12 +1,2 @@
-async function fetchText() {
-    let response = await fetch('/getMedicine');
+const { Script } = require("vm");
 
-    console.log(response.status); 
-    console.log(response.statusText); 
-
-    if (response.status === 200) {
-        let data = await response.text();
-       
-    }
-}
-fetchText();
